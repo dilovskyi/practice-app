97a3a26549158d429f51eb1c3f950c57f873084b
Dmytryi Kozlovskyi <dmytryikozlovskyi@MacBook-Air-Dmytryi.local>
2021-10-21 18:16:08 +0300

    Added rendering of all tasks from db.json

--- src/components/GeneratorsPage/CollapseList/CollapseList.js
+++ src/components/GeneratorsPage/CollapseList/CollapseList.js
@@ -1,17 +1,27 @@
+import { useState, useEffect } from "react";
+import { getData } from "../../../services/getData";
 import { Collapse, Divider, Space } from "antd";
 import ModalDataEntry from "../ModalDataEntry";
 import findHandler from "../../../helpers/findHandler";
 
 const { Panel } = Collapse;
 
-function CollapseList({ data, title }) {
+function CollapseList({ endpoint, title }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData(`http://localhost:3000/${endpoint}`).then((data) => {
+      setData(data);
+      console.log(data);
+    });
+  }, []);
+
   return (
     <>
       <Divider orientation="left">{title}</Divider>
       <Collapse>
         {data.map((item, index) => {
           const { id, type, description, handlerName, handlerParams } = item;
-
           return (
             <Panel header={handlerName} key={index}>
               <Space size={"middle"}>
--- src/components/GeneratorsPage/GeneratorsPage.js
+++ src/components/GeneratorsPage/GeneratorsPage.js
@@ -1,21 +1,11 @@
-import { useState, useEffect } from "react";
-import { getData } from "../../services/getData";
 import CollapseList from "./CollapseList";
 
 function GeneratorsPage() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    getData("http://localhost:3000/homework_1").then((data) => {
-      setData(data);
-    });
-  }, []);
-
   return (
     <>
-      <CollapseList data={data} title={"Домашняя работа №1"} />
-      <CollapseList data={data} title={"Домашняя работа №2"} />
-      <CollapseList data={data} title={"Домашняя работа №3"} />
+      <CollapseList endpoint={"homework_1"} title={"Домашняя работа №1"} />
+      <CollapseList endpoint={"homework_2"} title={"Домашняя работа №2"} />
+      <CollapseList endpoint={"homework_3"} title={"Домашняя работа №3"} />
     </>
   );
 }
