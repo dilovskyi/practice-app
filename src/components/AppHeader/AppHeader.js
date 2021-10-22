8fbab029bac88852f292f0557cf07bbc14d0d47d
Dmytryi Kozlovskyi <dmytryikozlovskyi@MacBook-Air-Dmytryi.local>
2021-10-23 00:12:06 +0300

    Added dynamic generation of menu items in AppHeader

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Menu } from "antd";
-import { SettingOutlined, SortAscendingOutlined } from "@ant-design/icons";
+import { SettingOutlined } from "@ant-design/icons";
 import { PageHeader } from "antd";
 import { Link } from "react-router-dom";
 import { Trans } from "react-i18next";
@@ -10,6 +10,7 @@ import ChangeLanguage from "../ChangeLanguage";
 function AppHeader({ changeLangHandler }) {
   const [current, setCurrent] = useState("generate");
   const [pageType, setPageType] = useState("generate");
+  const menuItems = ["generate", "compare"];
 
   const toggleActiveItem = (e) => {
     setCurrent(e.key);
@@ -26,28 +27,30 @@ function AppHeader({ changeLangHandler }) {
         onBack={() => window.history.back()}
         title={<Trans i18nKey={`${pageType}Page.title`} />}
         subTitle={<Trans i18nKey={`${pageType}Page.subTitle`} />}
-        extra={[
+        extra={
           <ChangeLanguage
             languages={LANGUAGES}
             changeLangHandler={changeLangHandler}
-          />,
-        ]}
+          />
+        }
       >
         <Menu
           onClick={toggleActiveItem}
           selectedKeys={[current]}
           mode="horizontal"
         >
-          <Menu.Item key="generate" icon={<SettingOutlined />}>
-            <Link to="/generate" onClick={() => setPageTypeHandler("generate")}>
-              <Trans i18nKey="headerItems.generate" />
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="compare" icon={<SortAscendingOutlined />}>
-            <Link to="/compare" onClick={() => setPageTypeHandler("compare")}>
-              <Trans i18nKey="headerItems.compare" />
-            </Link>
-          </Menu.Item>
+          {menuItems.map((itemName) => {
+            return (
+              <Menu.Item key={itemName} icon={<SettingOutlined />}>
+                <Link
+                  to={`/${itemName}`}
+                  onClick={() => setPageTypeHandler(itemName)}
+                >
+                  <Trans i18nKey={`headerItems.${itemName}`} />
+                </Link>
+              </Menu.Item>
+            );
+          })}
         </Menu>
       </PageHeader>
     </>
