ead5737dc6bdc4a0b1bd31129dc365d4d19363f9
Dmytryi Kozlovskyi <dmytryikozlovskyi@MacBook-Air-Dmytryi.local>
2021-10-23 17:48:43 +0300

    Fixed tasks import from homeworks

--- src/helpers/findHandler.js
+++ src/helpers/findHandler.js
@@ -1,9 +1,9 @@
-import * as homework_1 from "../vanillaJs/homework_1/homework_1";
+import { vanillaJsTasks } from "../vanillaJs";
 
 const findHandler = (handlerName) => {
-  for (let key in homework_1) {
+  for (let key in vanillaJsTasks) {
     if (handlerName === key) {
-      return homework_1[key];
+      return vanillaJsTasks[key];
     }
   }
 };
