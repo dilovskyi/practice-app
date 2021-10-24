50e05aba7eeb77c4b79852ccd7ef99c070387a4a
Dmytryi Kozlovskyi <dmytryikozlovskyi@MacBook-Air-Dmytryi.local>
2021-10-24 21:32:45 +0300

    Added translation of the ResultBaner

--- src/components/ResultBaner/ResultBaner.js
+++ src/components/ResultBaner/ResultBaner.js
@@ -1,10 +1,15 @@
 import { Result } from "antd";
+import { Trans } from "react-i18next";
 import { SmileOutlined } from "@ant-design/icons";
 
 function ResultBaner({ result }) {
   return (
     <>
-      <Result icon={<SmileOutlined />} title="Ваш рузультать:" extra={result} />
+      <Result
+        icon={<SmileOutlined />}
+        title={<Trans i18nKey="resultBaner.title" />}
+        extra={result}
+      />
     </>
   );
 }
