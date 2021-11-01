5a2ce4540e2fdb21532b6bb30a7ad1b4484adcdf
Dmytro Kozlovskyi <dmytrokozlovskyi@gmail.com>
2021-11-01 14:55:36 +0200

    SinglyList. Deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg'
-import './App.css'
-import { fillArrayByZero, fillByZeroOrOne } from './MyComponent'
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -20,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
